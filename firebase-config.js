c177f3d2ea11f13e17b126972320c5a518ca76c4
seiya-totoro <oguseiyaogu@icloud.com>
2026-07-11 12:11:24 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,12 @@
-// Firebase Realtime Databaseを使う場合だけ、この値をFirebaseの設定に差し替えてください。
-// nullのままだと、ローカル試作用としてlocalStorageに保存されます。
-window.SWINGBEANS_FIREBASE_CONFIG = null;
+window.SWINGBEANS_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCxEHfRA_FReAktpu_pfezUeCroSV9FsVI",
+  authDomain: "jazz-august.firebaseapp.com",
+  databaseURL: "https://jazz-august-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jazz-august",
+  storageBucket: "jazz-august.firebasestorage.app",
+  messagingSenderId: "238909338092",
+  appId: "1:238909338092:web:d72e36fe3653bf54e290c9",
+  measurementId: "G-JYP4DGDJHB"
+};
 
-// 例:
-// window.SWINGBEANS_FIREBASE_CONFIG = {
-//   apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-//   authDomain: "your-project.firebaseapp.com",
-//   databaseURL: "https://your-project-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "your-project",
-//   storageBucket: "your-project.appspot.com",
-//   messagingSenderId: "000000000000",
-//   appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx",
-// };
-
-// 同じFirebaseプロジェクト内で保存場所を変えたい場合だけ変更します。
 window.SWINGBEANS_FIREBASE_ROOT = "swingbeans/2026-08";
